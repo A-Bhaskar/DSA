b8a26b3778be562745c40572616de9503cbcff43
A-Bhaskar <67115288+A-Bhaskar@users.noreply.github.com>
2024-04-22 10:54:20 +0530

    Dijkstra sortest_path_algos.py

--- Graph/sortest_path_algos.py
+++ Graph/sortest_path_algos.py
@@ -1,1 +1,31 @@
 
+'''
+Dijkstra
+To find the minimum distance between the source to other nodes in the weighted graph
+'''
+from collections import defaultdict, deque
+from heapq import *
+def dijkstra(adj,source):
+  '''
+  type adj: dict of list
+  type source: int
+  '''
+  dist = difaultdict(int)
+  heap = []
+  vis = set()
+  dict[source] = 0
+  heappush(heap,(0,source))
+  while heap:
+    curr, distance = heappop(heap)
+    if curr in vis:
+      continue
+    vis.add(curr)
+    for neighbor,weight in adj[curr]:   ----> o(E)
+      if neighbor not in dis or dis[neighbor]>weight+dis[curr]:
+        dis[neighbor] = weight+dis[curr]   
+        heappush(heap,(dis[neighbor],neighbor)) ----> o(logV)
+  '''
+  Total complexity is E*logV
+  it cant support negative weight edges
+  if nodes are not present in dis that means they are  unreachable from source
+  '''
